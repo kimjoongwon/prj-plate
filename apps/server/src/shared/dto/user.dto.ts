b5e233bdd5eb802967e623b7dc03f26d59b24762
kimjoongwon <kimjwally@gmail.com>
2024-05-21 17:04:08 +0900

    update: user.dto.ts

--- apps/server/src/shared/dto/user.dto.ts
+++ apps/server/src/shared/dto/user.dto.ts
@@ -1,16 +1,42 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ProfileDto, TenantDto, UserEntity } from '../modules';
+import { AbstractDto } from './abstract.dto';
+import {
+  EmailField,
+  PasswordField,
+  PhoneField,
+  StringField,
+} from '../decorators';
+import { Type } from 'class-transformer';
 
-export class UserDto extends UserEntity {
-  constructor(partial: Partial<UserDto>) {
-    super(partial);
+export class UserDto extends AbstractDto implements UserEntity {
+  constructor(user: UserEntity) {
+    super(user);
+    this.email = user.email;
+    this.name = user.name;
+    this.phone = user.phone;
+    this.password = user.password;
   }
 
+  @EmailField()
+  email: string;
+
+  @StringField()
+  name: string;
+
+  @PhoneField()
+  phone: string;
+
+  @PasswordField()
+  password: string;
+
+  @Type(() => TenantDto)
   @ApiProperty({
     type: () => [TenantDto],
   })
   tenants: TenantDto[];
 
+  @Type(() => ProfileDto)
   @ApiProperty({
     type: () => [ProfileDto],
   })
