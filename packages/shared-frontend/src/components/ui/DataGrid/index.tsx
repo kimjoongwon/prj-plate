eaee999086af887e29f777f8827a4ec9c0ec925a
kimjoongwon <kimjwally@gmail.com>
2024-12-13 15:50:05 +0900

    feat: table 정리

--- packages/shared-frontend/src/components/ui/DataGrid/index.tsx
+++ packages/shared-frontend/src/components/ui/DataGrid/index.tsx
@@ -15,12 +15,9 @@ import {
   GroupingState,
   useReactTable,
 } from '@tanstack/react-table';
-import { renderButton } from '../../renderer/renderButton';
 import {
   Selection,
-  ButtonProps,
   SortDescriptor,
-  Spacer,
   Table,
   TableBody,
   TableCell,
@@ -29,10 +26,8 @@ import {
   TableProps,
   TableRow,
 } from '@nextui-org/react';
-import { TableContainer } from './TableContainer';
-import { HStack } from '../HStack';
 import { observer } from 'mobx-react-lite';
-import { Pagination } from '../Pagination';
+import { action } from 'mobx';
 
 export type Key = string | number;
 
@@ -55,8 +50,6 @@ export interface DataGridProps<T> extends TableProps {
   state?: DataGridState<T>;
   data: T[];
   columns: ColumnDef<T, any>[];
-  leftButtons?: ButtonProps[];
-  rightButtons?: ButtonProps[];
   hideButtons?: boolean;
   actions?: ('삭제' | '생성' | '제거')[];
   selectedKey?: string;
@@ -75,13 +68,9 @@ export const DataGrid = observer(
     const {
       data,
       columns = [],
-      leftButtons = [],
-      rightButtons = [],
       selectedKey = 'id',
       state,
       emptyContent = '데이터가 없습니다.',
-      totalCount,
-      hideButtons,
     } = props;
 
     const [columnVisibility, setColumnVisibility] = React.useState({});
@@ -117,74 +106,51 @@ export const DataGrid = observer(
       debugColumns: false,
     });
 
-    if (!table) return null;
-
-    if (columns.length === 0) {
-      throw new Error('columns is empty');
-    }
-
     const headers = table?.getHeaderGroups?.()?.[0]?.headers || [];
     const rows = table?.getRowModel?.()?.rows || [];
 
+    const onSelectionChange = action((selection: Selection) => {
+      state.selection = selection;
+    });
+
     return (
-      <TableContainer>
-        {!hideButtons && (
-          <>
-            <HStack className="w-full">
-              <HStack className="w-full justify-start">
-                {leftButtons?.map(renderButton)}
-              </HStack>
-              <HStack className="w-full justify-end space-x-2">
-                {rightButtons?.map(renderButton)}
-              </HStack>
-            </HStack>
-            <Spacer y={1} />
-          </>
-        )}
-        <Table
-          defaultSelectedKeys={state?.selection}
-          onSelectionChange={(selection: Selection) => {
-            state.selection = selection;
+      <Table
+        defaultSelectedKeys={state?.selection}
+        onSelectionChange={onSelectionChange}
+      >
+        <TableHeader columns={headers}>
+          {header => {
+            return (
+              <TableColumn key={header.column.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+              </TableColumn>
+            );
           }}
-        >
-          <TableHeader columns={headers}>
-            {header => {
-              return (
-                <TableColumn key={header.column.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
+        </TableHeader>
+        <TableBody emptyContent={emptyContent} items={rows}>
+          {row => {
+            return (
+              <TableRow key={row.original?.[selectedKey as keyof T]}>
+                {row.getVisibleCells().map(cell => {
+                  return (
+                    <TableCell key={cell?.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
                       )}
-                </TableColumn>
-              );
-            }}
-          </TableHeader>
-          <TableBody emptyContent={emptyContent} items={rows}>
-            {row => {
-              return (
-                <TableRow key={row.original?.[selectedKey as string]}>
-                  {row.getVisibleCells().map(cell => {
-                    return (
-                      <TableCell key={cell?.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext(),
-                        )}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            }}
-          </TableBody>
-        </Table>
-        <Spacer y={4} />
-        {!!state?.query?.take && (
-          <Pagination totalCount={totalCount} state={state?.query} />
-        )}
-      </TableContainer>
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            );
+          }}
+        </TableBody>
+      </Table>
     );
   },
 );
