579ad06e438a1774d0b2a4ccb5a2fdfd2af72a4f
kimjoongwon <kimjwally@gmail.com>
2025-06-04 14:52:04 +0900

    refactor: remove unused constants and error handling code for cleaner structure

--- packages/shared-frontend/index.ts
+++ packages/shared-frontend/index.ts
@@ -1,7 +1,6 @@
 export * from './src/components';
 export * from './src/hooks';
 export * from './src/providers';
-export * from './src/errors';
 export * from './src/services';
 
 export * as ComponentManager from './src/components';
--- packages/shared-frontend/src/errors/index.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosError } from 'axios';
-
-export class InvalidPasswordError {
-  readonly _tag = 'InvalidPasswordError';
-}
-
-export class GalaxyError<D> extends AxiosError<unknown, D> {
-  readonly _tag = 'GalaxyError';
-}
