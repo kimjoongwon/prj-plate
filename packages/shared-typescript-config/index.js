57efec614cf5e6c1e3d6104af2f53206d0ab3fbf
kimjoongwon <kimjwally@gmail.com>
2025-07-12 16:11:29 +0900

    refactor: clean up formatting in TypeScript configuration files and remove unused node.json

--- packages/shared-typescript-config/index.js
+++ packages/shared-typescript-config/index.js
@@ -1,3 +1,2 @@
-export const base = require('./base.json');
-export const react = require('./react.json');
-export const node = require('./node.json');
+export const base = require("./base.json");
+export const react = require("./react.json");
